104cb82454006297be35c7ef798fff0194b03e61
nanjuxi <1367064846@qq.com>
2023-03-15 14:27:40 +0000

    第二次提交

--- jssp.py
+++ jssp.py
@@ -2,57 +2,59 @@
 import linecache
 import numpy as np
 
-class Jssp:
+class Jssp(object):
     def __init__(self, path):
         self.path = path
-        # self.read_dataset(path)
+        self.read_dataset(path)
 
-    
-    def txt_to_matrix(path):
-        file=open(path)
-        lines=file.readlines()
-        #print lines
-        #['0.94\t0.81\t...0.62\t\n', ... ,'0.92\t0.86\t...0.62\t\n']形式
-        rows=len(lines)#文件行数
- 
-        datamat=np.zeros((rows,15))#初始化矩阵
-  
-        row=0
-        for line in lines:
-            line=line.strip().split('\t')#strip()默认移除字符串首尾空格或换行符
-            datamat[row,:]=line[:]
-            row+=1
-        return datamat
-    
+    def read_dataset(self,path):
 
-    def read_dataset(path):
+        # f = open(path)
+        text = linecache.getline(path, 2).split()
+        n = int(text[0])
+        m = int(text[1])
+        times=np.zeros((n,m))
+        row1=4
+        while(row1<4+n):
+            line = linecache.getline(path, row1).split()
+            times[row1-4,:]=line[:]
+            row1+=1
+        machines=np.zeros((n,m))
+        row2=5+n
+        while(row2<20+n):
+            line = linecache.getline(path, row2).split()
+            machines[row2-20,:]=line[:]
+            row2+=1
+        self.m = m
+        self.n = n
+        self.times = times
+        self.machines = machines
 
-        f = open(path)
-        text = linecache.getline(path, 2)
-        n = text[1]
-        print(text)
-
-        m1 = []
-
-
-        m=0
-        n=0
-        machine = [[1,2,3],[1,2,3]]
-        time =[]
-        return m,n,machine,time
+    def jssp_solve(self,x):
+        work_machine = []
+        for i in range(self.m):
+            work_machine.append([])
+        for i in range(self.n):
+            for j in range(self.m):
+                k = int(self.machines[i][j])
+                work_machine[k-1].append([i,j,self.times[i][j]])
 
 
-
-    def jssp_solve(x):
         makespan=0
+
         return makespan
 
 if __name__=="__main__":
-    j = Jssp.txt_to_matrix("./Tailard15_15.txt")
-    print(j)
+    j = Jssp("./Tailard15_15.txt")
     # makespan = j.jssp_solve([[1,2,3],[1,2,3],[1,2,3]])
     # print(makespan)
+    x = 0
+    i = j.jssp_solve(x)
    
 
-
+# machine1的工作队列[
+# [(1,14,t),(2,11)(),(),()],
+# [()()()()],
+# [()()()],
+# ]
 
