caacaefae76b718b6f122c2b290ba7915afb8d60
nanjuxi <1367064846@qq.com>
2023-03-22 15:45:12 +0000

    第六次提交，改进穷举求解器，解决内存溢出的问题

--- exhaustive_solve.py
+++ exhaustive_solve.py
@@ -1,33 +1,36 @@
 from jssp import Jssp 
 import itertools
-class ExhausivelSolve(object):
-    def __init__(self):
-        pass
 
-    def solve(self,jp:Jssp):
+def DiGui(n):
+    if n==1:
+        return 1
+    return n*DiGui(n-1)
+
+class ExhausivelSolve(object):
+    def __init__(self,jp:Jssp):
+        self.p_iter = []
+        self.x = []
+        self.jp = jp
+        for i in range(0,jp.m):
+            self.p_iter.append(itertools.permutations([i for i in range(1,jp.n + 1)]))
+            self.x.append(next(self.p_iter[i]))
+
+
+    def solve(self):
+        jp =self.jp
         t_min = 99999999999
         x_fal = []
-        x = []
-
-        x_all_iter= itertools.permutations([i for i in range(1,jp.n + 1)])
-        x_all = []
-        for i in x_all_iter:
-            x_all.append(i)
-        k = len(x_all)
-
+        k = DiGui(jp.n)
         p = [0 for i in range(0,jp.m)]
         count = 0
         count_sum = k**jp.m
         while(True):
-            x = []
-            for i in p:
-                x.append(x_all[i])
             count = count +1
-            t = jp.jssp_solve(x)
-
+            t = jp.jssp_solve(self.x)
+            # print(self.x)
             if t < t_min:
                 t_min = t
-                x_fal = x
+                x_fal = self.x
             
             
             p[jp.m-1]=p[jp.m-1]+1
@@ -38,18 +41,21 @@ class ExhausivelSolve(object):
                 if p[idx] >= k:
                     inv =1 
                     p[idx] = p[idx]-k 
+                    self.p_iter[idx] = itertools.permutations([i for i in range(1,jp.n + 1)])
+                    self.x[idx] = next(self.p_iter[idx])
                     idx = idx-1
                 else:
+                    self.x[idx] = next(self.p_iter[idx])
                     inv = 0
                     break
             
             if p == [0 for i in range(0,jp.m)]:
                 break
-            if count % 100 == 0:
+            if count % 1 == 0:
                 print(f"穷举进度: {count}/{count_sum}",end='\r')
         
-        print(f"总共有{count}个x矩阵")
-        print(x_fal,t_min)
+        # print(f"总共有{count}个x矩阵")
+        # print(x_fal,t_min)
         return x_fal,t_min
 
         x_all = itertools.permutations([i for i in range(1,jssp.n + 1)])
@@ -59,8 +65,9 @@ class ExhausivelSolve(object):
 
 if __name__ == "__main__":
     j = Jssp("./Tailard15_15.txt")
-    solver = ExhausivelSolve()
+    # j = Jssp("./test.txt")
+    solver = ExhausivelSolve(j)
 
-    x,t =  solver.solve(j)
-    print(x)
-    print(t)
+    x,t =  solver.solve()
+    # print(x)
+    # print(t)
